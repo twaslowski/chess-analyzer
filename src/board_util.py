89d57f1448698c02be55f6f76ce2e1768e14cf4d
Tobias Waslowski <tobiaswaslowski@gmail.com>
2021-09-27 18:27:01 +0200

    i hate whitespaces

--- src/board_util.py
+++ src/board_util.py
@@ -6,7 +6,7 @@ def generate_turn_string(move_counter: float, move_algebraic: str, use_context: 
     # when black moves, the notation can either be 10. ..b4 or just b4, depending on context
     if move_counter % 1 != 0:
         prefix = f"{math.floor(move_counter)}. .." if use_context else ''
-        return f"{prefix}{move_algebraic}"
+        return f"{prefix}{move_algebraic} "
     # white to move
     else:
-        return f'{int(move_counter)}. {move_algebraic}'
+        return f'{int(move_counter)}. {move_algebraic} '
