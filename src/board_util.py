6bd9d8efce4f0285b092e54ba2ff4a39f31c491e
Tobias Waslowski <tobiaswaslowski@gmail.com>
2021-09-27 11:18:54 +0200

    fix

--- src/board_util.py
+++ src/board_util.py
@@ -3,7 +3,7 @@ import math
 
 def generate_turn_string(move_counter: float, move_algebraic: str, use_context: bool) -> str:
     if move_counter % 1 != 0:
-        prefix = f"{math.floor(move_counter)}. .." if use_context else ' '
+        prefix = f"{math.floor(move_counter)}. .." if use_context else ''
         return f"{prefix}{move_algebraic} "
     else:
         return f'{int(move_counter)}. {move_algebraic} '
