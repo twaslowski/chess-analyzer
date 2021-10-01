adb10b0540cf9c21e551b0e51c4957d043a77fd3
Tobias Waslowski <tobiaswaslowski@gmail.com>
2021-10-01 12:53:29 +0200

    Update bash scripts

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Installing pip dependencies ..."
+python3 -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
 
 echo "Compiling stockfish ..."
--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ if [[ -z "${telegram_token_chess}" ]]; then
   echo "No token was found for deployment. Exiting."
   exit 1
 else
-	nohup src/main.py &
+  source venv/bin/activate
+	nohup code/src/main.py &
 	echo $! > pid.txt
 fi
